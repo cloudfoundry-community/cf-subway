30ff2e8ff73ec17f9fbb311a6fcc3324242cc17d
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-15 22:13:44 -0700

    add root certs into container

--- scripts/stop-backend-brokers.sh
+++ scripts/stop-backend-brokers.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+sudo apt-get update
+sudo apt-get install -y ca-certificates
+
 cf login -a ${api} -u ${username} -p ${password} -o ${organization} -s ${space}
 
 cf a | grep started | awk '{print $1}' | xargs -L1 cf stop
